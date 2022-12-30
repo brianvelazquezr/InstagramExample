978eb59772f831766eaadd2ac29450e66067b4d1
BVR <brian.extrastuff@hotmail.com>
2022-12-30 16:21:59 -0600

    dependency injection added

--- app/src/main/java/com/brianvelazquez/jetpackcomposeinstagram/login/data/LoginRepository.kt
+++ app/src/main/java/com/brianvelazquez/jetpackcomposeinstagram/login/data/LoginRepository.kt
@@ -2,9 +2,9 @@ package com.brianvelazquez.jetpackcomposeinstagram.login.data
 
 import android.provider.ContactsContract.CommonDataKinds.Email
 import com.brianvelazquez.jetpackcomposeinstagram.login.data.network.LoginService
+import javax.inject.Inject
 
-class LoginRepository {
-    private val api = LoginService()
+class LoginRepository @Inject constructor(private val api : LoginService){
 
     suspend fun doLogin(email: String, password: String): Boolean{
         return api.doLogin(email = email, password = password)
